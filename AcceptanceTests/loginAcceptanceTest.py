7f227cef1c2f00a8f7f988cb1966c95e2a8424a4
Chris <chrisfaber07@gmail.com>
2022-12-03 19:08:54 -0600

    Added acceptance Test for login

--- AcceptanceTests/loginAcceptanceTest.py
+++ AcceptanceTests/loginAcceptanceTest.py
@@ -5,13 +5,16 @@ class LoginTest(TestCase):
     tester = null
 
     def setup(self):
-
+        tester = Client()
         User(1234, username, password, first, last,ContactInfo("email@abc.com" , "123456789", Address("street1", "street2" , "city", "state", "12345")))
 
     def test_loginSuccess(self):
-
-        pass
+        response = tester.post('/login/', {"username": "username", "password": "password"}, follow= true)
+        self.assertRedircts(response, 'index', msg= "Did not redirect to home page")
+        self.assertEqual(response.session["username"], "username", msg= "Session key not equal to User's username")
     def test_incorrectUsername(self):
-        pass
+        response = tester.post('/login/', {"username": "notUsername", "password": "password"}, follow=true)
+        self.assertEqual(response.context["message"], "username or password incorrect, try again!", msg="wrong username doesn't display the message username or password incorrect, try again!")
     def test_incorrectPassword(self):
-        pass
+        response = tester.post('/login/', {"username": "username", "notPassword": "password"}, follow=true)
+        self.assertEqual(response.context["message"], "username or password incorrect, try again!", msg="wrong password doesn't display the message username or password incorrect, try again!")
