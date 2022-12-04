5c5128bc47cd92e8e145b01e6e8881700a4db4b8
seehu <seehu@uwm.edu>
2022-12-03 19:10:26 -0600

    edited UnitTests to conform with group feedback time:2

--- UnitTests/courseUnitTests.py
+++ UnitTests/courseUnitTests.py
@@ -52,37 +52,45 @@ class TestEditCourse(unittest.TestCase):
         Scheduler.models.Course(number="CS361", semester="Fall", year=2022)
 
     def test_SuccessfulEdit(self):
-        Course.editCourse("CS458", "Spring", 2023)
+        Course.editCourse("CS361", "CS458", "Spring", 2023)
         course458 = Course.objects.get(number="CS458")
         self.assertEqual(course458.number, "CS458", msg="Course Number not updated correctly")
         self.assertEqual(course458.semester, "Spring", msg="Semester not updated correctly")
         self.assertEqual(course458.year, 2023, msg="Year not updated correctly")
 
     def test_NullEdit(self):
-        Course.editCourse(None, None, None)
-        course361 = Course.objects.get(number="CS361")
-        self.assertEqual(course361.number, "CS361", msg="Course Number should not have updated to None")
+        Course.editCourse("CS361", None, None, None)
+        with self.assertRaises(NameError, msg="Course not found, Number shouldn't have updated"):
+            course361 = Course.objects.get(number="CS361")
         self.assertEqual(course361.semester, "Fall", msg="Semester should not have updated to None")
         self.assertEqual(course361.year, 2022, msg="Year should not have updated to None")
 
     def test_PartialEdit(self):
-        Course.editCourse(None, "Fall", None)
-        course361 = Course.objects.get(number="CS361")
-        self.assertEqual(course361.number, "CS361", msg="Course Number should not have updated to None")
+        Course.editCourse("CS361", None, "Fall", None)
+        with self.assertRaises(NameError, msg="Course not found, Number shouldn't have updated"):
+            course361 = Course.objects.get(number="CS361")
         self.assertEqual(course361.semester, "Fall", msg="Semester should have updated to Fall")
         self.assertEqual(course361.year, 2022, msg="Year should not have updated to None")
 
+    def test_NoSearchName(self):
+        with self.assertRaises(TypeError, msg="Must provide a valid course"):
+            Course.editCourse(None, "CS458", "Fall", 2022)
+
+    def test_WrongSearchName(self):
+        with self.assertRaises(TypeError, msg="Must provide a valid course"):
+            Course.editCourse("CS999", "CS458", "Fall", 2022)
+
     def test_InvalidArg(self):
         with self.assertRaises(TypeError, msg="wrong Arg Type"):
-            Course.editCourse(Course, "Fall", 2022)
+            Course.editCourse("CS361", Course, "Fall", 2022)
 
     def test_InvalidArg2(self):
         with self.assertRaises(TypeError, msg="wrong Arg Type"):
-            Course.editCourse("CS361", Course, 2022)
+            Course.editCourse("CS361", "CS458", Course, 2022)
 
     def test_InvalidArg3(self):
         with self.assertRaises(TypeError, msg="wrong Arg Type"):
-            Course.editCourse("CS361", "Fall", "Dino Nuggets")
+            Course.editCourse("CS361", "CS458", "Fall", "Dino Nuggets")
 
     def test_NoArg(self):
         with self.assertRaises(TypeError, msg="not enough Arg"):
@@ -96,9 +104,13 @@ class TestEditCourse(unittest.TestCase):
         with self.assertRaises(TypeError, msg="not enough Arg"):
             Course.editCourse("CS361", 2022)
 
-    def test_FourArg(self):
+    def test_ThreeArg(self):
         with self.assertRaises(TypeError, msg="not enough Arg"):
-            Course.editCourse("CS361", "Fall", 2022, "Banana Pickles")
+            Course.editCourse("CS361", "CS458", 2022)
+
+    def test_FiveArg(self):
+        with self.assertRaises(TypeError, msg="not enough Arg"):
+            Course.editCourse("CS361", "C458", "Fall", 2022, "Banana Pickles")
 
     def test_InvalidName(self):
         with self.assertRaises(KeyError, msg="Key(Course) already exists"):
