975297b802106e351cdd99bf1f0b54455ba051b8
connornethen <CLNethen@uwm.edu>
2022-12-03 19:20:08 -0600

    Created Login in views.py

--- Scheduler/views.py
+++ Scheduler/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from Scheduler.models import User
+
+
 class Login(View):
     def get(self, request):
-        pass
+        return render(request, "login.html", {"message": None})
+
     def post(self, request):
-        pass
+        validCred = True
+        myUsername = request.POST['username']
+        myPassword = request.POST['password']
+        bad_user = False
+        bad_password = False
+
+        try:
+            user = User.objects.get(name=myUsername)
+            bad_password = (user.password != myPassword)
+        except:
+            bad_user = True
+
+        if bad_user or bad_password:
+            validCred = False
+
+        if validCred:
+            request.session["username"] = myUsername
+            return redirect("/index/")
+        else:
+            return render(request, "login.html", {"message": "Username or password is incorrect, try again!"})
