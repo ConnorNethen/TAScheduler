805aeb2c5693fc11e3ab76f56ca74e30c3b80b6e
Usama Khan <usamaarshadkhan@gmail.com>
2022-12-05 17:29:55 -0600

    Implemented contactInfo class

--- classes/contactInfo.py
+++ classes/contactInfo.py
@@ -1,19 +1,30 @@
+from Scheduler.models import ContactInfo
+from address import Address
+
 class ContactInfo():
-    def setUniqueID(self):
-        pass
+    def __init__(self, email, phone, street1, street2, city, state, zip):
+        self.email = email
+        self.phone = phone
+        self.street1 = street1
+        self.street2 = street2
+        self.city = city
+        self.state = state
+        self.zip = zip
 
-    def setEmail(self):
-        pass
+    def save(self):
+        address = Address(street1=self.street1, street2=self.street2, city=self.city, state=self.state, zip=self.zip)
+        address.save()
+        ContactInfo.objects.create(email=self.email, phone=self.phone, address=address)
 
-    def setPhone(self):
-        pass
+    def setEmail(self, email):
+        self.email = email
 
-    def setAddress(self):
-        pass
+    def setPhone(self, phone):
+        self.phone = phone
 
-    def setUser(self):
-        pass
-
+    def setAddress(self, address):
+        self.address = address
+        
     def getUniqueID(self):
         pass
 
