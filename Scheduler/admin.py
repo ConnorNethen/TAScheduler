f93d6dfb074b4ef8dc277a371d9ce5996a8dffa3
Usama Khan <usamaarshadkhan@gmail.com>
2022-12-05 13:22:28 -0600

    Updated project settings to include Scheduler app

--- Scheduler/admin.py
+++ Scheduler/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Address, ContactInfo, Status, User, Course, Section, UserToSection
 
 # Register your models here.
+admin.site.register(Address)
+admin.site.register(ContactInfo)
+admin.site.register(Status)
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(UserToSection)
