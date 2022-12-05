d690e46fd23b926e1fd956b07af37d3ec6571aaf
Usama Khan <usamaarshadkhan@gmail.com>
2022-12-05 13:56:47 -0600

    Updated users.py

--- classes/user.py
+++ classes/user.py
@@ -1,41 +1,111 @@
+from Scheduler.models import User, Address, ContactInfo, Status, Course, Section, UserToSection
 
 class User:
-    def setPantherID(self):
-        pass
+    def __init__(self, pID, username, password, fname, lname, email, phone, street1, street2, city, state, zip, status):
+        self.pID = pID
+        self.username = username
+        self.password = password
+        self.fname = fname
+        self.lname = lname
+        self.contactInfo = ContactInfo(email=email, phone=phone, address=Address(street1=street1, street2=street2, city=city, state=state, zip=zip))
+        self.email = email
+        self.phone = phone
+        self.street1 = street1
+        self.street2 = street2
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.status = status
 
-    def setFirstName(self):
-        pass
+    def save(self):
+        address = Address(street1=self.street1, street2=self.street2, city=self.city, state=self.state, zip=self.zip)
+        address.save()
+        contactInfo = ContactInfo(email=self.email, phone=self.phone, address=address)
+        contactInfo.save()
+        status = Status(status=self.status)
+        status.save()
+        user = User(pID=self.pID, username=self.username, password=self.password, fname=self.fname, lname=self.lname, contactInfo=contactInfo, status=status)
+        user.save()
 
-    def setLastName(self):
-        pass
-
-    def setAddress(self):
-        pass
-
-    def setContactInfo(self):
-        pass
-
-    def setStatus(self):
-        pass
-
-    def getPantherID(self):
-        pass
-
+    # Getters
     def getFirstName(self):
-        pass
+        return self.fname
 
     def getLastName(self):
-        pass
+        return self.lname
 
-    def getAddress(self):
-        pass
+    def getEmail(self):
+        return self.email
 
-    def getContactInfo(self):
-        pass
+    def getPhone(self):
+        return self.phone
+
+    def getStreet1(self):
+        return self.street1
+        
+    def getStreet2(self):
+        return self.street2
+
+    def getCity(self):
+        return self.city
+
+    def getState(self):
+        return self.state
+
+    def getZip(self):
+        return self.zip
 
     def getStatus(self):
-        pass
+        return self.status
 
-    
+    def getUsername(self):
+        return self.username
 
+    def getPassword(self):
+        return self.password
 
+    def getAddresses(self):
+        return Address.objects.get(street1=self.street1, street2=self.street2, city=self.city, state=self.state, zip=self.zip)
+
+    def getContactInfo(self):
+        return ContactInfo.objects.get(email=self.email)
+
+    # Setters
+    def setPID(self, pID):
+        self.pID = pID
+
+    def setFirstName(self, fname):
+        self.fname = fname
+
+    def setLastName(self, lname):
+        self.lname = lname
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setPhone(self, phone):
+        self.phone = phone
+
+    def setStreet1(self, street1):
+        self.street1 = street1
+
+    def setStreet2(self, street2):
+        self.street2 = street2
+
+    def setCity(self, city):
+        self.city = city
+
+    def setState(self, state):
+        self.state = state
+
+    def setZip(self, zip):
+        self.zip = zip
+
+    def setStatus(self, status):
+        self.status = status
+
+    def setUsername(self, username):
+        self.username = username
+
+    def setPassword(self, password):
+        self.password = password
