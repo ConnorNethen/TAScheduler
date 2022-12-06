d2cb11e7fcd9d8e3eb84b4c56dd1120188bdbd5d
Usama Khan <usamaarshadkhan@gmail.com>
2022-12-06 10:47:58 -0600

    Updated urls to work with views as classes

--- Scheduler/urls.py
+++ Scheduler/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('users/', views.all_users, name='all_users'),
-    path('users/new/', views.new_user, name='new_user'),
-    path('courses/', views.all_courses, name='all_courses'),
-    path('courses/new/', views.new_course, name='new_course'),
-    path('courses/<str:course_number>/', views.course, name='course'),
-    path('courses/<str:course_number>/new-section/', views.new_section, name='new_section'),
+    path('', views.Index.as_view(), name='index'),
+    path('login/', views.Login.as_view(), name='login'),
+    # path('logout/', views.logout, name='logout'),
+    path('users/', views.AllUsers.as_view(), name='all_users'),
+    path('users/new/', views.CreateUser.as_view(), name='create_user'),
+    # path('courses/', views.all_courses, name='all_courses'),
+    path('courses/new/', views.CreateCourse.as_view(), name='create_course'),
+    # path('courses/<str:course_number>/', views.course, name='course'),
+    # path('courses/<str:course_number>/new-section/', views.new_section, name='new_section'),
 ]
