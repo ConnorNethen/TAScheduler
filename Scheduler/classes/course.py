c4e38fca973b6dc60ea1f5f6e4f67f2588fd67b0
connornethen <CLNethen@uwm.edu>
2022-12-11 21:09:31 -0600

    Need to finish getUsers, getSection, setSection, and removeCourse

---
+++ Scheduler/classes/course.py
@@ -0,0 +1,39 @@
+
+
+class AppCourse():
+    sectionID = None
+    courseID = None
+    userPID = None
+
+    def __init__(self, courseID, name, semester, year):
+        pass
+
+    def getCourseID(self):
+        pass
+
+    def getName(self):
+        pass
+
+    def getSemester(self):
+        pass
+
+    def getYear(self):
+        pass
+
+    def setName(self, name):
+        pass
+
+    def setSemester(self, semester):
+        pass
+
+    def setYear(self, year):
+        pass
+
+    def getUsers(self):
+        pass
+
+    def getSection(self):
+        pass
+
+    def removeCourse(self):
+        pass
--- tests/unit_tests/test_course.py
+++ tests/unit_tests/test_course.py
@@ -1,117 +1,180 @@
 from django.test import TestCase
-from Scheduler.models import Course
-from Scheduler.classes import app_user
+from Scheduler.classes.course import AppCourse
+from Scheduler.models import Course, UserCourse, Section
+from Scheduler.classes import app_user, course, section
 
 
 class CourseCreationTests(TestCase):
     def test_Init(self):
-        Course361 = Course("CS361", "Introduction to Software Engineering", "F", 2022)
-        Course361.save()
-        self.assertTrue(Course.objects.filter(CourseID="CS361").exists())
+        myCourse = AppCourse("CS361", "Introduction to Software Engineering", "F", 2022)
+        self.assertEqual(myCourse.CourseID, Course.objects.get(courseID="CS361").courseID, msg= "Error")
+        self.assertEqual(myCourse.name, Course.objects.get(name="Introduction to Software Engineering"))
+        self.assertEqual(myCourse.semester, Course.objects.get(semster="F"))
+        self.assertEqual(myCourse.year, Course.objects.get(year=2022))
 
     def test_No_Arguments(self):
-        with self.assertRaises(ValueError, msg="Not Enough Arguments Given"):
-            Course01 = Course()
+        with self.assertRaises(TypeError, msg="Not Enough Arguments Given"):
+            Course01 = AppCourse()
 
     def test_One_Argument(self):
-        with self.assertRaises(ValueError, msg="Not Enough Arguments Given"):
-            Course01 = Course("CS361")
+        with self.assertRaises(TypeError, msg="Not Enough Arguments Given"):
+            Course01 = AppCourse("CS361")
 
     def test_Two_Arguments(self):
-        with self.assertRaises(ValueError, msg="Not Enough Arguments Given"):
-            Course01 = Course("CS361", "Introduction to Software Engineering")
+        with self.assertRaises(TypeError, msg="Not Enough Arguments Given"):
+            Course01 = AppCourse("CS361", "Introduction to Software Engineering")
 
     def test_Three_Arguments(self):
-        with self.assertRaises(ValueError, msg="Not Enough Arguments Given"):
-            Course01 = Course("CS361", "Introduction to Software Engineering", "F")
+        with self.assertRaises(TypeError, msg="Not Enough Arguments Given"):
+            Course01 = AppCourse("CS361", "Introduction to Software Engineering", "F")
 
     def test_More_Arguments(self):
-        with self.assertRaises(ValueError, msg="Too Many Arguments Given"):
-            Course01 = Course("CS361", "Introduction to Software Engineering", "F", 2022, "Something")
+        with self.assertRaises(TypeError, msg="Too Many Arguments Given"):
+            Course01 = AppCourse("CS361", "Introduction to Software Engineering", "F", 2022, "Something")
 
     def test_Invalid_Id_Type(self):
-        with self.assertRaises(ValueError, msg="Invalid CourseID Argument"):
-            CourseA = Course(361, "Introduction to Software Engineering", "F", 2022)
+        with self.assertRaises(TypeError, msg="Invalid CourseID Argument"):
+            CourseA = AppCourse(361, "Introduction to Software Engineering", "F", 2022)
 
     def test_Invalid_Id_Value(self):
-        with self.assertRaises(TypeError, msg="Invalid CourseID Argument"):
-            CourseA = Course("CS361.5", "Introduction to Software Engineering", "F", 2022)
+        with self.assertRaises(ValueError, msg="Invalid CourseID Argument"):
+            CourseA = AppCourse("CS361.5", "Introduction to Software Engineering", "F", 2022)
 
     def test_Repeated_Id(self):
-        Course01 = Course("CS361", "Intro", "F", 2022)
-        with self.assertRaises(ValueError, msg="CourseID is Not Unique"):
-            Course02 = Course("CS361", "Systems Programing", "F", 2022)
+        Course01 = AppCourse("CS361", "Intro", "F", 2022)
+        with self.assertRaises(TypeError, msg="CourseID is Not Unique"):
+            Course02 = AppCourse("CS361", "Systems Programing", "F", 2022)
 
 
 class CourseGetTests(TestCase):
     def setup(self):
-        self.Course01 = Course("CS361", "Introduction to Software Engineering", "F", 2022)
+        self.Course01 = AppCourse("CS361", "Introduction to Software Engineering", "F", 2022)
         self.Course01.save()
-        self.Course02 = Course("CS337", "Systems Programming", "S", 2023)
-        self.Course02.save()
 
     def test_getCourseID(self):
         self.assertEqual(self.Course01.getCourseID(), "CS361")
-        self.assertEqual(self.Course02.getCourseID(), "CS337")
+
+    def test_getCourseID_ManyArg(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.getCourseID("CS361")
 
     def test_getName(self):
         self.assertEqual(self.Course01.getName(), "Introduction to Software Engineering")
-        self.assertEqual(self.Course02.getName(), "Systems Programming")
+
+    def test_getName_ManyArg(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.getName("Systems Programming")
 
     def test_getSemester(self):
         self.assertEqual(self.Course01.getSemester(), "F")
-        self.assertEqual(self.Course02.getSemester(), "S")
+
+    def test_getSemester_ManyArg(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.getSemester("U")
 
     def test_getYear(self):
         self.assertEqual(self.Course01.getYear(), 2022)
-        self.assertEqual(self.Course02.getYear(), 2023)
+
+    def test_getYear_ManyArg(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.getYear(2022)
 
 
 class CourseSetTests(TestCase):
     def setup(self):
-        self.Course01 = Course("CS361", "Introduction to Software Engineering", "F", 2022)
+        self.Course01 = AppCourse("CS361", "Introduction to Software Engineering", "F", 2022)
         self.Course01.save()
-        self.Course02 = Course("CS337", "Systems Programming", "S", 2023)
-        self.Course02.save()
 
     def test_setName(self):
         self.Course01.setName("Conclusion of Software Engineering")
-        self.Course02.setName("Computer Programming")
+        self.Course01.save()
         self.assertEqual(Course.objects.get(CourseID="CS361").name, "Conclusion of Software Engineering")
-        self.assertEqual(Course.objects.get(CourseID="CS337").name, "Computer Programming")
+
+    def test_setName_NoArgs(self):
+        with self.assertRaises(TypeError, msg="No arguments passed into function"):
+            self.Course01.setName()
+
+    def test_setName_ManyArgs(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.setName("Introduction", "to Software Engineering")
 
     def test_setSemester(self):
         self.Course01.setSemester("U")
         self.Course01.save()
-        self.Course02.setSemester("W")
-        self.Course02.save()
         self.assertEqual(Course.objects.get(CourseID="CS361").semester, "U")
-        self.assertEqual(Course.objects.get(CourseID="CS337").semester, "W")
+
+    def test_setSemester_NoArgs(self):
+        with self.assertRaises(TypeError, msg="No arguments passed into function"):
+            self.Course01.setSemester()
+
+    def test_setSemester_ManyArgs(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.setSemester("F", "S")
 
     def test_setYear(self):
         self.Course01.setYear(2025)
         self.Course01.save()
-        self.Course02.setYear(2025)
-        self.Course02.save()
         self.assertEqual(Course.objects.get(CourseID="CS361").year, 2025)
-        self.assertEqual(Course.objects.get(CourseID="CS337").year, 2025)
+
+    def test_setYear_NoArgs(self):
+        with self.assertRaises(TypeError, msg="No arguments passed into function"):
+            self.Course01.setYear()
+
+    def test_setYear_ManyArgs(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed into function"):
+            self.Course01.setYear(2022, 2023)
 
 
 class TestGetUsers(TestCase):
     def setUp(self):
-        # create a couple users
-        self.user = app_user(123, "user@test.com", "pass")
-        self.user2 = app_user(456, "user2@test.com", "pass")
+        # create users
+        user01 = app_user("123", "user01@test.com", "pass")
+        user01.save()
+        user02 = app_user("456", "user02@test.com", "pass")
+        user02.save()
+        user03 = app_user("789", "user03@test.com", "pass")
+        user03.save()
 
-        # create a couple courses
-        Course01 = Course(courseID="CS 361 XX", name="Intro to SE", semester="F", year=2022)
-        Course01.save()
+        # create courses
+        self.Course01 = AppCourse(courseID="CS 361", name="Intro to SE", semester="F", year=2022)
+        self.Course01.save()
 
-        b = Course(courseID="CS 337 XX", name="System Programming", semester="F", year=2022)
+        # create connections
+        a = UserCourse(user="123", course="CS 361")
+        a.save()
+        b = UserCourse(user="456", course="CS 361")
         b.save()
-
-        c = Course(courseID="CS 361 X", name="Intro to SE", semester="F", year=2022)
+        c = UserCourse(user="789", course="CS 361")
         c.save()
+
+    def test_successful_call(self):
+        listOfUsers = self.Course01.getUsers()
+        self.assertIsInstance(listOfUsers[0], app_user, msg="Error creating instance")
+        self.assertEqual(listOfUsers[0].pID, "123")
+        self.assertIsInstance(listOfUsers[1], app_user, msg="Error creating instance")
+        self.assertEqual(listOfUsers[1].pID, "456")
+        self.assertIsInstance(listOfUsers[0], app_user, msg="Error creating instance")
+        self.assertEqual(listOfUsers[2].pID, "789")
+
+    def test_invalid_course(self):
+        with self.assertRaises(TypeError, msg="Course does not exist"):
+            listOfUsers = self.Course02.getUsers()
+
+
+class TestGetSection(TestCase):
+    def setup(self):
+        pass
+
+
+class TestSetSection(TestCase):
+    def setup(self):
+        pass
+
+
+class TestRemoveCourse(TestCase):
+    def setup(self):
+        self.Course01 = AppCourse(courseID="CS 361", name="Intro to SE", semester="F", year=2022)
+        self.Course02 = AppCourse(courseID="CS 337", name="Systems Programming", semester="F", year=2022)
 
 
 
